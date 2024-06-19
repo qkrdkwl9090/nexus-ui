38f84a3f2ddb0ad668feee9e2adcb40c70ac89f5
Tony <qkrdkwl9090@naver.com>
2024-06-19 21:16:00 +0900

    feat: set eslint(with Test Button comp)

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import typescriptEslintParser from "@typescript-eslint/parser";
+import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
+import reactPlugin from "eslint-plugin-react";
+
+const config = [
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      ecmaVersion: 2018,
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        module: "readonly",
+        document: "readonly",
+        console: "readonly",
+        require: "readonly",
+        process: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslintPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
+
+export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
-console.log(1);
+import Button from './components/Button';
+export { Button };
