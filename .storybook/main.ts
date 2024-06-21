b101c71382f474aeac7cb060c92e73555514f106
Tony <qkrdkwl9090@naver.com>
2024-06-21 20:07:50 +0900

    fix(storybook): change return type to string(lint)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,7 @@ import { join, dirname } from 'path';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
@@ -23,7 +23,7 @@ const config: StorybookConfig = {
     getAbsolutePath('@storybook/addon-interactions')
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {}
   },
   docs: {
