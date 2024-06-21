cfd1feaf4b0cfba419c09e4f7153bc89da05c1c5
Tony <qkrdkwl9090@naver.com>
2024-06-21 20:14:09 +0900

    chore(eslint): set ignores in eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,38 @@
-import typescriptEslintParser from "@typescript-eslint/parser";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
-import reactPlugin from "eslint-plugin-react";
+import typescriptEslintParser from '@typescript-eslint/parser';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import reactPlugin from 'eslint-plugin-react';
 
 const config = [
   {
-    files: ["**/*.{js,jsx,ts,tsx}"],
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['tsconfig.json', 'eslint.config.js', 'dist/**'],
     languageOptions: {
       parser: typescriptEslintParser,
       ecmaVersion: 2018,
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
-        window: "readonly",
-        module: "readonly",
-        document: "readonly",
-        console: "readonly",
-        require: "readonly",
-        process: "readonly",
-      },
+        window: 'readonly',
+        module: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        require: 'readonly',
+        process: 'readonly'
+      }
     },
     plugins: {
-      "@typescript-eslint": typescriptEslintPlugin,
-      react: reactPlugin,
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: reactPlugin
     },
     rules: {
       ...typescriptEslintPlugin.configs.recommended.rules,
-      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules
     },
     settings: {
       react: {
-        version: "detect",
-      },
-    },
-  },
+        version: 'detect'
+      }
+    }
+  }
 ];
 
 export default config;
